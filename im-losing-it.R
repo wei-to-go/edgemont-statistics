bdcb612e42a7e80bb415325583860d9ffe039e91
Wei Dewdney <realzhengwei06@gmail.com>
2022-09-25 23:42:02 -0400

    finished draft plots, I definitely won't be able to clean them up before the article is due so I'm praying to lucifer that I'll get an extension

--- im-losing-it.R
+++ im-losing-it.R
@@ -1,5 +1,6 @@
 ## downloading needed libraries
 library(dplyr)
+library(ggplot2)
 
 ##compiling dataframs
 
@@ -18,7 +19,7 @@ gradRace <- data.frame(matrix(ncol = 3, nrow = 6))
 colnames(gradRace) <- c('race','nys_grad_rate','edgemont_grad_rate')
 gradRace$race <- c('native_american_or_alaska_native','african_american','hispanic_latinx', 'AAPI', 'white', 'multiracial')
 gradRace$nys_grad_rate <- c(82,92,80,80,90,84)
-gradRace$edgemont_grad_rate <- c(NA,100,100,92,100,100)
+gradRace$edgemont_grad_rate <- c(NA,100,100,100,92,100)
 View(gradRace)
 
 #edgemont racial breakdown past 2 decades
@@ -37,9 +38,66 @@ View(edgemont20race)
 ##making plots, basic analysis
 
 #racial breakdown comparison
-
+ggplot(ethnicityCompare, aes(x=race, y=nys_percent))+
+  geom_point()+
+  geom_point(aes(x=race, y=edgemont_percent))
 
 #grad rate comparison
+ggplot(gradRace, aes(x=race, y=nys_grad_rate))+
+  geom_point()+
+  geom_point(aes(x=race, y=edgemont_grad_rate))
 
-
-#
+#ehs racial breakdown across 2 decades
+ggplot(edgemont20race, aes(x=year, y=white, group=1))+
+  geom_point()+
+  geom_path()+
+  geom_point(aes(y=african_american, group=1))+
+  geom_path(aes(y=african_american))+
+  geom_point(aes(y=asian, group=1))+
+  geom_path(aes(y=asian))+
+  geom_point(aes(y=hispanic_latinx, group=1))+
+  geom_path(aes(y=hispanic_latinx))+
+  geom_point(aes(y=native_american, group=1))+
+  geom_path(aes(y=native_american))+
+  geom_point(aes(y=pacific_islander, group=1))+
+  geom_path(aes(y=pacific_islander))+
+  geom_point(aes(y=multiracial, group=1))+
+  geom_path(aes(y=multiracial))
+              
+ggplot(edgemont20race, aes(x=year, y=hispanic_latinx, group=1))+
+  geom_point()+
+  geom_path()+
+  geom_point(aes(y=native_american, group=1))+
+  geom_path(aes(y=native_american))+
+  geom_point(aes(y=pacific_islander, group=1))+
+  geom_path(aes(y=pacific_islander))+
+  geom_point(aes(y=multiracial, group=1))+
+  geom_path(aes(y=multiracial))
+  
+##diversity score
+diversity <- mutate(edgemont20race,
+                    diversity_score = 1-((white*(white-1)+
+                                            african_american*(african_american-1)+
+                                            asian*(asian-1)+
+                                            hispanic_latinx*(hispanic_latinx-1)+
+                                            native_american*(native_american-1)+
+                                            pacific_islander*(pacific_islander-1)+
+                                            multiracial*(multiracial-1))
+                                         /((white+
+                                              african_american+
+                                              asian+
+                                              hispanic_latinx+
+                                              native_american+
+                                              pacific_islander+
+                                              multiracial)*
+                                             (white+
+                                                african_american+
+                                                asian+hispanic_latinx+
+                                                native_american+
+                                                pacific_islander+
+                                                multiracial-1))))
+diversity <- select(diversity, year, diversity_score)
+View(diversity)
+ggplot(diversity, aes(x=year, y=diversity_score, group=1))+
+  geom_point()+
+  geom_path()
