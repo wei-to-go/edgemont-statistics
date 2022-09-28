827a808cbbaffc8e3e1a1cdf45c4c182d60b2744
Wei Dewdney <realzhengwei06@gmail.com>
2022-09-27 21:30:36 -0400

    almost done :D

--- im-losing-it.R
+++ im-losing-it.R
@@ -18,8 +18,8 @@ View(ethnicityCompare)
 gradRace <- data.frame(matrix(ncol = 3, nrow = 6))
 colnames(gradRace) <- c('race','nys_grad_rate','edgemont_grad_rate')
 gradRace$race <- c('native_american_or_alaska_native','african_american','hispanic_latinx', 'AAPI', 'white', 'multiracial')
-gradRace$nys_grad_rate <- c(82,92,80,80,90,84)
-gradRace$edgemont_grad_rate <- c(NA,100,100,100,92,100)
+gradRace$nys_grad_rate <- c(82,80,80,92,90,84)
+gradRace$edgemont_grad_rate <- c(NA,100,92,100,100,100)
 View(gradRace)
 
 #edgemont racial breakdown past 2 decades
@@ -58,35 +58,81 @@ ggplot(ethnicityCompare, aes(x=race, y=nys_percent))+
 
 #grad rate comparison
 ggplot(gradRace, aes(x=race, y=nys_grad_rate))+
-  geom_point()+
-  geom_point(aes(x=race, y=edgemont_grad_rate))
+  geom_point(aes(color='NYS'), shape=18, size=3)+
+  geom_point(aes(x=race, y=edgemont_grad_rate, color='EHS'), shape=18, size=3)+
+  scale_color_manual(name='legend',
+                     labels=c('NYS','EHS'),
+                     values=c('NYS'='red','EHS'='blue'))+
+  labs(title='Figure 4 - grad rates for various ethnicities',
+       y = 'grad rate')+
+  scale_x_discrete(labels=c('AAPI','Black','Latinx', 'Multiracial', 'Indigenous','White'))+
+  theme_minimal()+
+  theme(legend.position=c(.745,.7), 
+        legend.background = element_rect(fill = "gray"),   
+        legend.key = element_rect(fill = "lightgray", color='white'),
+        title = element_text(face='bold', family='mono'),
+        legend.text = element_text(family='serif'),
+        axis.text.x = element_text(family='serif', margin=margin(1,1,9,1)),
+        axis.text.y = element_text(family='serif', margin=margin(1,1,1,9)))
 
 #ehs racial breakdown across 2 decades
-ggplot(edgemont20race, aes(x=year, y=white, group=1))+
+ggplot(edgemont20race, aes(x=year, y=white, group=1, color='w'))+
   geom_point()+
   geom_path()+
-  geom_point(aes(y=african_american, group=1))+
-  geom_path(aes(y=african_american))+
-  geom_point(aes(y=asian, group=1))+
-  geom_path(aes(y=asian))+
-  geom_point(aes(y=hispanic_latinx, group=1))+
-  geom_path(aes(y=hispanic_latinx))+
-  geom_point(aes(y=native_american, group=1))+
-  geom_path(aes(y=native_american))+
-  geom_point(aes(y=pacific_islander, group=1))+
-  geom_path(aes(y=pacific_islander))+
-  geom_point(aes(y=multiracial, group=1))+
-  geom_path(aes(y=multiracial))
+  geom_point(aes(y=african_american, group=1, color='aa'))+
+  geom_path(aes(y=african_american, color='aa'))+
+  geom_point(aes(y=asian, group=1, color='a'))+
+  geom_path(aes(y=asian, color='a'))+
+  geom_point(aes(y=hispanic_latinx, group=1, color='hislat'))+
+  geom_path(aes(y=hispanic_latinx, color='hislat'))+
+  geom_point(aes(y=native_american, group=1, color='na'))+
+  geom_path(aes(y=native_american, color='na'))+
+  geom_point(aes(y=pacific_islander, group=1, color='pi'))+
+  geom_path(aes(y=pacific_islander, color='pi'))+
+  geom_point(aes(y=multiracial, group=1, color='mr'))+
+  geom_path(aes(y=multiracial, color='mr'))+
+  scale_color_manual(name='race',
+                     labels=c('White','African-American','Asian','Latinx','Indigenous','Pacific Islander','Multiracial'),
+                     values=c('w'='red', 'aa'='darkorange2','a'='goldenrod1','hislat'='chartreuse3','na'='blue','pi'='purple','mr'='magenta'))+
+  theme_minimal()+
+  labs(title='Figure 2 - EHS racial distribution timeline',
+       y='number of students')+
+  theme(legend.position=c(.18,.55), 
+        legend.key.size = unit(.39, "cm"),
+        legend.background = element_rect(fill = "gray"),   
+        legend.key = element_rect(fill = "white", color='black'),
+        title = element_text(face='bold', family='mono'),
+        legend.text = element_text(family='serif'),
+        axis.text.x = element_text(family='serif', size=8, angle=45),
+        axis.text.y = element_text(family='serif', margin=margin(1,1,1,6)))
               
-ggplot(edgemont20race, aes(x=year, y=hispanic_latinx, group=1))+
+ggplot(edgemont20race, aes(x=year, y=hispanic_latinx, group=1, color='hislat'))+
   geom_point()+
   geom_path()+
-  geom_point(aes(y=native_american, group=1))+
-  geom_path(aes(y=native_american))+
-  geom_point(aes(y=pacific_islander, group=1))+
-  geom_path(aes(y=pacific_islander))+
-  geom_point(aes(y=multiracial, group=1))+
-  geom_path(aes(y=multiracial))
+  geom_point(aes(y=african_american, group=1, color='aa'))+
+  geom_path(aes(y=african_american, color='aa'))+
+  geom_point(aes(y=native_american, group=1, color='na'))+
+  geom_path(aes(y=native_american, color='na'))+
+  geom_point(aes(y=pacific_islander, group=1, color='pi'))+
+  geom_path(aes(y=pacific_islander, color='pi'))+
+  geom_point(aes(y=multiracial, group=1, color='mr'))+
+  geom_path(aes(y=multiracial, color='mr'))+
+  scale_color_manual(name='race',
+                      labels=c('Latinx','African-American','Indigenous','Pacific Islander','Multiracial'),
+                      values=c('aa'='darkorange2','hislat'='chartreuse3','na'='blue','pi'='purple','mr'='magenta'))+
+  theme_minimal()+ 
+  labs(title='figure 3 - minority populations',
+        y='number of students')+
+  scale_y_continuous(breaks=seq(from=0, to=80, by=10))+
+  theme(legend.position=c(.194,.74), 
+         legend.key.size = unit(.39, "cm"),
+        legend.background = element_rect(fill = "gray"),   
+        legend.key = element_rect(fill = "white", color='black'),
+        title = element_text(face='bold', family='mono'),
+        legend.text = element_text(family='serif'),
+        axis.text.x = element_text(family='serif', size=8, angle=45),
+        axis.text.y = element_text(family='serif', margin=margin(1,1,1,6)))
+    
   
 ##diversity score
 diversity <- mutate(edgemont20race,
@@ -114,4 +160,34 @@ diversity <- select(diversity, year, diversity_score)
 View(diversity)
 ggplot(diversity, aes(x=year, y=diversity_score, group=1))+
   geom_point()+
-  geom_path()
+  geom_path()+
+  labs(title='figure 5 - diversity score timeline',
+       y='diversity score')+
+  theme_minimal()+
+  theme(legend.position=c(.194,.74), 
+        legend.key.size = unit(.39, "cm"),
+        legend.background = element_rect(fill = "gray"),   
+        legend.key = element_rect(fill = "white", color='black'),
+        title = element_text(face='bold', family='mono'),
+        legend.text = element_text(family='serif'),
+        axis.text.x = element_text(family='serif', size=8, angle=45),
+        axis.text.y = element_text(family='serif', margin=margin(1,1,1,6)))+
+  annotate("text", x = 17, y = .56, 
+           label = "plateau",
+           color = "red", fontface = 2)+
+  annotate("text", x = 8.5, y = .46, 
+           label = "plateau",
+           color = "red", fontface = 2)+
+  annotate("text", x = 19, y = .44, 
+           label = "possible increase,
+  similar to 2010-2012 ?",
+           color = "purple", size=3, fontface = 1)+
+  geom_segment(x = 19, y = .47, xend = 21, yend = .62, color='purple',
+               arrow = arrow(length = unit(0.5, "cm")))+
+  annotate('text',x=8.8,y=.41,
+           label='increase', color='blue', fontface=2)+
+  geom_segment(x = 8.5, y = .4, xend = 6.6, yend = .35, color='blue',
+               arrow = arrow(length = unit(0.2, "cm")))+
+  geom_curve(x = 10.1, y = .41, xend = 11.5, yend = .52, color='blue',
+               arrow = arrow(length = unit(0.2, "cm")))
+  
